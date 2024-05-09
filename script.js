09ae17b1771c759a957476bb2938b63ce442ba9e
Brandon Bartlett <153580357+BrandB123@users.noreply.github.com>
2024-05-08 21:58:29 -0500

    Add class to number buttons

---
+++ script.js
@@ -0,0 +1,38 @@
+let x;
+let y;
+let operator;
+
+// button eventListeners
+let number = document.querySelector(".number");
+
+mainContainer.addEventListener("click", (event) => {
+    let target = event.target.id;
+
+});
+
+// updateDisplay()
+
+// operate()
+
+
+
+
+// add functionality so buttons update display
+
+/* add functionality so that selecting an operator submits the numbers in the display to a variable
+and the operator is saved in a variable*/
+
+/* add functionality so the equals button submits the display to a variable and calls a function 
+to perform an equation*/
+
+
+
+// VARIABLES
+/*
+x : value submitted by selecting an operator (this will also capture the result of operations)
+y: value submitted by selecting the equals button 
+operator: obtained when operator button is selected
+*/
+
+
+// OPERATE() FUNCTION
